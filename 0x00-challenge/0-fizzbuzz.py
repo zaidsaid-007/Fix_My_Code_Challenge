34203c6c430018d3256d6c63dedfe75bd06e2a19
zaidsaid-007 <zaidsaid007@gmail.com>
2024-03-23 16:43:01 +0300

    initial commit

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -21,6 +21,7 @@ def fizzbuzz(n):
             tmp_result.append("FizzBuzz")
         elif (i % 3) == 0:
             tmp_result.append("Fizz")
+        
         elif (i % 5) == 0:
             tmp_result.append("Buzz")
         else:
