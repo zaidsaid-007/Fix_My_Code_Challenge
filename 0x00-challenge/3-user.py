e13d57d88e459e7c824707752620c437503643d6
zaidsaid-007 <zaidsaid007@gmail.com>
2024-03-23 16:56:39 +0300

    user.py changes

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -48,13 +48,13 @@ class User():
         - `False` if `pwd` is `None`
         - `False` if `pwd` is not a string
         - `False` if `__password` is `None`
-        - Compare `__password` and the MD5 value of `pwd`
+        - Compare lowercase MD5 hash of `pwd` and the lowercase stored password hash
         """
         if pwd is None or type(pwd) is not str:
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 
 
 if __name__ == '__main__':
